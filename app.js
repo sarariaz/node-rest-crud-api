bd8711768f2cbddafee90eef3b2b72dbcd754cfe
sara riaz <srq147147@gmail.com>
2019-09-14 09:41:39 -0700

    Node-rest-crud-api

--- app.js
+++ app.js
@@ -35,24 +35,26 @@ app.get("/dogs", function(req,res){
     });
 
 });
-// fetch single dog
-app.get("/dogs/:id", function(req, res){
-   let user_id = req.params.id;
-   if(!user_id){
-       res.status(400).send({error: true, message: "Please provide the Dog ID"});
 
-   }
-   dbConn.query('SELECT * FROM dogs where id=?', user_id, function(err, results, fields){
-       if(err){
-           throw err;
-       }
-       res.send({error: false , data: results[0] ,message: 'Specific Dog List '});
-   });
-});
-// new dogs form route
+// new dogs form 
 app.get("/dogs/new", function(req,res){
     res.render("form.ejs");
 });
+
+// fetch single dog
+app.get("/dogs/:id", function(req, res){
+    let user_id = req.params.id;
+    if(!user_id){
+        res.status(400).send({error: true, message: "Please provide the Dog ID"});
+ 
+    }
+    dbConn.query('SELECT * FROM dogs where id=?', user_id, function(err, results, fields){
+        if(err){
+            throw err;
+        }
+        res.send({error: false , data: results[0] ,message: 'Specific Dog List '});
+    });
+ });
 
 //add new user (create)
 app.post("/dogs", function(req,res){
@@ -71,8 +73,39 @@ app.post("/dogs", function(req,res){
      }
 )});
 
+//UPDATE USER
+app.put("/dogs", function(req,res){
+    var user = {
+        id: req.body.id,
+        name: req.body.name
+    };
+    if(!user){
+       res.status(400).send({ error: true, message: "Dog Name or Dog ID is missing"});
+
+    }
+    dbConn.query("UPDATE dogs SET name= ? WHERE id= ?", [user.name, user.id], function(err, results,fields){
+        if(err){
+            throw err;
+        }
+        return res.send({error: false, data: results, message: "Dog has been updated succesfully"});
+    });
+});
+
+//Delete User
+app.delete("/dogs", function (req, res) {
+    let user_id = req.body.id;
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    dbConn.query('DELETE FROM dogs WHERE id = ?', [user_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Dog has been deleted successfully.' });
+    });
+});
 
  // set port
  app.listen(3000, function () {
     console.log('Node app is running on port 3000');
 });
+
+module.exports = app;
